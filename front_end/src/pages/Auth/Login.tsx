9592f67fd862f8767f316ada7a0b4692907fdd67
PH50447truong <truong-truongnvph50447@gmail.com>
2025-06-04 18:56:21 +0700

    ui đăng kí

--- front_end/src/pages/Auth/Login.tsx
+++ front_end/src/pages/Auth/Login.tsx
@@ -29,7 +29,7 @@ const Login = () => {
 
         setTimeout(() => {
           if (data.user.role === 'admin') {
-            navigate('/admin');
+            navigate('/dashboard');
           } else {
             navigate('/');
           }
@@ -42,55 +42,75 @@ const Login = () => {
   };
 
   return (
-    <div className="h-screen w-screen flex justify-center items-center dark:bg-gray-900">
+    <div className=" w-screen flex justify-center items-center dark:bg-gray-900">
       <div className="grid gap-8">
-        <div className="bg-gradient-to-r from-blue-500 to-purple-500 rounded-[26px] m-4">
-          <div className="border-[20px] border-transparent rounded-[20px] dark:bg-gray-900 bg-white shadow-lg p-10 m-2">
-            <h1 className="pt-8 pb-6 font-bold dark:text-gray-400 text-5xl text-center">Log in</h1>
+        <div className=" m-4">
+          <div className=" p-10 m-2">
+           
 
             {message && <p className="text-center text-red-500 mb-4">{message}</p>}
 
-            <form onSubmit={handleLogin} className="space-y-4">
-              <div>
-                <label htmlFor="phone" className="mb-2 dark:text-gray-400 text-lg">Số điện thoại</label>
+            <form onSubmit={handleLogin} className="bg-[#7f7cb4] p-8 rounded-lg flex flex-col md:flex-row items-center text-white space-y-6 md:space-y-0 md:space-x-10">
+              {/* //cột trái logo chữ */}
+              <div className="space-y-2 text-5xl font-extrabold leading-tight text-center">
+                <div className="bg-[url('https://i.ibb.co/ZJxF2jw/mosaic.jpg')] bg-cover bg-clip-text text-transparent">
+                  SECOND
+                </div>
+               
+              </div>
+              {/* // cột phải form đăng nhập */}
+
+              <div className="w-80">
+                <h2 className="text-2xl font-semibold mb-6 text-center">ĐĂNG NHẬP</h2>
+               
+
                 <input
-                  id="phone"
-                  className="border p-3 shadow-md dark:bg-indigo-700 dark:text-gray-300 dark:border-gray-700 border-gray-300 rounded-lg w-full"
+                id="phone"
                   type="text"
                   value={phone}
                   onChange={(e) => setPhone(e.target.value)}
-                  placeholder="Số điện thoại"
+                  placeholder="Nhập số điện thoại"
+                  className="w-full px-4 py-2 mb-4 rounded-md text-black focus:outline-none"
                   required
                 />
-              </div>
 
-              <div>
-                <label htmlFor="password" className="mb-2 dark:text-gray-400 text-lg">Password</label>
-                <input
-                  id="password"
-                  className="border p-3 shadow-md dark:bg-indigo-700 dark:text-gray-300 dark:border-gray-700 border-gray-300 rounded-lg w-full"
-                  type="password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  placeholder="Password"
-                  required
-                />
-              </div>
+                <div className="relative mb-4">
+                  <input
+                   id="password"
+                    type="password"
+                     value={password}
+                    placeholder="Nhập mật khẩu"
+                     onChange={(e) => setPassword(e.target.value)}
+                    className="w-full px-4 py-2 rounded-md text-black focus:outline-none"
+                    required
+                  />
+                  <span className="absolute right-3 top-2.5 text-gray-500 cursor-pointer">👁️</span>
+                </div>
+           
 
-              <button
-                className="bg-gradient-to-r dark:text-gray-300 from-blue-500 to-purple-500 shadow-lg mt-6 p-2 text-white rounded-lg w-full hover:scale-105 hover:from-purple-500 hover:to-blue-500 transition duration-300 ease-in-out"
-                type="submit"
-              >
-                LOG IN
-              </button>
+                <button
+                  type="submit"
+                  className="w-full py-2 bg-[#9f86c0] hover:bg-[#a892c9] text-white font-semibold rounded-md mb-4"
+                >
+                  Xác nhận
+                </button>
+
+                <div className="flex items-center my-4">
+                  <hr className="flex-grow border-white" />
+                  <span className="px-3 text-white">hoặc</span>
+                  <hr className="flex-grow border-white" />
+                </div>
+
+                <p className="text-center text-sm">
+                  Bạn chưa có tài khoản?
+                  <a href="#" className="text-[#dcc4f2] hover:underline">Đăng ký</a>
+                </p>
+              </div>
+              {/* kthuc */}
+              
             </form>
 
-            <div className="flex flex-col mt-4 items-center justify-center text-sm">
-              <h3 className="dark:text-gray-300">
-                Don't have an account?
-                <a className="text-blue-400 ml-2 underline" href="/register">Sign Up</a>
-              </h3>
-            </div>
+           
           </div>
         </div>
       </div>
