d75226d760cb0f25b36c6b7bae9a724c323563bd
PH50447truong <truong-truongnvph50447@gmail.com>
2025-06-06 16:48:14 +0700

    tìm kiếm sản phẩm theo tên

--- front_end/src/routes/ClientRoutes.tsx
+++ front_end/src/routes/ClientRoutes.tsx
@@ -8,6 +8,9 @@ import ProductDetails from "../pages/Client/ProductDetails"
 import ProductList from "../pages/Client/ProductList"
 import Order from "../pages/Client/Order"
 import OrderNews from "../pages/Client/OrderNews"
+import Checkout from "../pages/Client/Checkout"
+import OrderSuccessfully from "../pages/Client/OrderSuccessfully"
+import SearchResults from "../pages/Client/SearchResults"
 
 const ClientRoutes = {
   path: '/',
@@ -21,6 +24,11 @@ const ClientRoutes = {
     { path: 'products', element: <ProductList /> },
     { path: 'orders', element: <Order/>},
     { path: 'ordernews', element: <OrderNews/>},
+
+     { path: 'checkout', element: <Checkout/>},
+    { path: 'ordersuccessfully', element: <OrderSuccessfully/>},
+    { path: 'search', element: <SearchResults /> },
+
 
     // Khi đường dẫn sai hoặc không tồn tại, dẫn đến trang NotFound
     { path: '*', element: <NotFound homePath="/"/> },
